5840e03e6a3946202a76ef74d75ec5da685fce0b
Dell-User <Dell-User@DESKTOP-8EU8MHV>
2023-10-12 14:02:56 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace First_Display
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLoanAmount = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDownPayment = new System.Windows.Forms.TextBox();
+            this.txtInterestRate = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTerms = new System.Windows.Forms.TextBox();
+            this.btnMortgage = new System.Windows.Forms.Button();
+            this.btnAuto = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(144, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Loan Amount:";
+            // 
+            // txtLoanAmount
+            // 
+            this.txtLoanAmount.Location = new System.Drawing.Point(323, 60);
+            this.txtLoanAmount.Name = "txtLoanAmount";
+            this.txtLoanAmount.Size = new System.Drawing.Size(184, 20);
+            this.txtLoanAmount.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(144, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Down Payment:";
+            // 
+            // txtDownPayment
+            // 
+            this.txtDownPayment.Location = new System.Drawing.Point(323, 104);
+            this.txtDownPayment.Name = "txtDownPayment";
+            this.txtDownPayment.Size = new System.Drawing.Size(184, 20);
+            this.txtDownPayment.TabIndex = 3;
+            // 
+            // txtInterestRate
+            // 
+            this.txtInterestRate.Location = new System.Drawing.Point(323, 142);
+            this.txtInterestRate.Name = "txtInterestRate";
+            this.txtInterestRate.Size = new System.Drawing.Size(184, 20);
+            this.txtInterestRate.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label3.Location = new System.Drawing.Point(144, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Interest Rate:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label4.Location = new System.Drawing.Point(144, 196);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Terms in Years:";
+            // 
+            // txtTerms
+            // 
+            this.txtTerms.Location = new System.Drawing.Point(323, 196);
+            this.txtTerms.Name = "txtTerms";
+            this.txtTerms.Size = new System.Drawing.Size(184, 20);
+            this.txtTerms.TabIndex = 7;
+            // 
+            // btnMortgage
+            // 
+            this.btnMortgage.Location = new System.Drawing.Point(323, 251);
+            this.btnMortgage.Name = "btnMortgage";
+            this.btnMortgage.Size = new System.Drawing.Size(75, 23);
+            this.btnMortgage.TabIndex = 8;
+            this.btnMortgage.Text = "Mortgage";
+            this.btnMortgage.UseVisualStyleBackColor = true;
+            this.btnMortgage.Click += new System.EventHandler(this.btnMortgage_Click);
+            // 
+            // btnAuto
+            // 
+            this.btnAuto.Location = new System.Drawing.Point(432, 251);
+            this.btnAuto.Name = "btnAuto";
+            this.btnAuto.Size = new System.Drawing.Size(75, 23);
+            this.btnAuto.TabIndex = 9;
+            this.btnAuto.Text = "Auto";
+            this.btnAuto.UseVisualStyleBackColor = true;
+            this.btnAuto.Click += new System.EventHandler(this.btnAuto_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(117, 301);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(174, 24);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Monthly Payment: $";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnAuto);
+            this.Controls.Add(this.btnMortgage);
+            this.Controls.Add(this.txtTerms);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtInterestRate);
+            this.Controls.Add(this.txtDownPayment);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtLoanAmount);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "LoanCalculators";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtLoanAmount;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDownPayment;
+        private System.Windows.Forms.TextBox txtInterestRate;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtTerms;
+        private System.Windows.Forms.Button btnMortgage;
+        private System.Windows.Forms.Button btnAuto;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("First Display")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("First Display")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1f2b241a-8189-4852-b975-935e04ef28aa")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
